4a1c752eb0aa2e1fa7da0c5d09b8d5159fb10cb8
22p31f0010 <dodlayogesh2512@gmail.com>
2023-11-02 18:06:50 +0530

    2nov23

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -6,8 +7,7 @@ class list(models.Model):
     name =models.CharField(max_length=250)
     age=models.IntegerField(default=1)
     address=models.CharField(max_length=250)
-
-
+    time=models.DateTimeField
 
     def __str__(self):
         return self.name
